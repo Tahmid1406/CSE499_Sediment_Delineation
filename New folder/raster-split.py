36a7d0e6bdec96943f1cb959b0e286775940d5b6
tahmid1406 <tahmidpranto1406@gmail.com>
2021-03-30 08:34:39 +0600

    final files

--- New folder/raster-split.py
+++ New folder/raster-split.py
@@ -9,5 +9,5 @@ tilesize = 256
 for i in range(0, width, tilesize):
     for j in range(0, height, tilesize):
         gdaltranString = "gdal_translate -of GTIFF -srcwin "+str(i)+", "+str(j)+", "+str(tilesize)+", " \
-            +str(tilesize)+" Jan19_B13.tif utm_"+str(i)+"_"+str(j)+".tif"
+            +str(tilesize)+" Dec19_B13.tif utm_"+str(i)+"_"+str(j)+".tif"
         os.system(gdaltranString)
